2e8cc1591e06221e15cf9f45f5ff83fcf9f5494e
AdrianKoretski <34486058+AdrianKoretski@users.noreply.github.com>
2020-02-02 10:49:21 -0500

    Simplified the abstract vertex shader.

--- Src/Pipeline/VertexShader/VertexShader.cpp
+++ Src/Pipeline/VertexShader/VertexShader.cpp
@@ -1,5 +1,4 @@
 #include "VertexShader.h"
-#include "..//..//glm/gtc/type_ptr.hpp"
 #include <iostream>
 VertexShader::VertexShader()
 {
@@ -7,14 +6,9 @@ VertexShader::VertexShader()
 
 void VertexShader::processVertices(buffer<float>& output_VBO, buffer<float>& input_VBO)
 {
-	buffer<float> output_data;
-	output_data.resize(m_output_vertex_size);
+	output_VBO.resize(m_output_vertex_size * input_VBO.size());
 	for (uint i = 0; i < input_VBO.size(); i += m_input_vertex_size)
-	{
-		shadeVertex(&output_data[0], &input_VBO[i]);
-		for (uint i = 0; i < m_output_vertex_size; i++)
-			output_VBO.push_back(output_data[i]);
-	}
+		shadeVertex(&output_VBO[i], &input_VBO[i]);
 }
 
 void VertexShader::shadeVertex(float* output, float* input)
